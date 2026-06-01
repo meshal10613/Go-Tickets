37a0dcac6a45db20e7f1861a2e794a5d89f12a75
meshal10613 <syedmohiuddinmeshal24@gmail.com>
2026-06-01 20:37:20 +0600

    feat: implementing request validation & install gorm for postgres

--- cmd/main.go
+++ cmd/main.go
@@ -1,26 +1,112 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"regexp"
 
 	"github.com/fatih/color"
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v5"
 	"github.com/labstack/echo/v5/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i any) error {
+	if err := cv.validator.Struct(i); err != nil {
+		// Optionally, you could return the error to give each route more control over the status code
+		return echo.ErrBadRequest.Wrap(err)
+	}
+	return nil
+}
+
+// func (cv *CustomValidator) Validate(i any) error {
+// 	return cv.validator.Struct(i)
+// }
+
+var (
+	upperRegex   = regexp.MustCompile(`[A-Z]`)
+	lowerRegex   = regexp.MustCompile(`[a-z]`)
+	numberRegex  = regexp.MustCompile(`[0-9]`)
+	specialRegex = regexp.MustCompile(`[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]`)
+)
+
+func passwordValidation(fl validator.FieldLevel) bool {
+	password := fl.Field().String()
+
+	return len(password) >= 8 &&
+		upperRegex.MatchString(password) &&
+		lowerRegex.MatchString(password) &&
+		numberRegex.MatchString(password) &&
+		specialRegex.MatchString(password)
+}
+
+type User struct {
+	Name  string `json:"name" form:"name" query:"name" validate:"required"`
+	Email string `json:"email" form:"email" query:"email" validate:"required,email"`
+	// Password string `json:"password" form:"password" query:"password" validate:"required,min=8,containsany=ABCDEFGHIJKLMNOPQRSTUVWXYZ,containsany=abcdefghijklmnopqrstuvwxyz,containsany=0123456789,containsany=!@#$%^&*()_+-=[]{}|;:,.<>?/"`
+	Password string `json:"password" form:"password" query:"password" validate:"required,password"`
+}
+
 func main() {
+	dsn := "host=localhost user=postgres password=4219 dbname=go-tickets port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	port := 5000
 	e := echo.New()
 	e.Use(middleware.RequestLogger())
+	validate := validator.New()
+	validate.RegisterValidation("password", passwordValidation)
+
+	e.Validator = &CustomValidator{
+		validator: validate,
+	}
 
 	e.GET("/", func(c *echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
+	e.POST("/users", func(c *echo.Context) error {
+		u := new(User)
+		if err := c.Bind(u); err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]any{"error": err.Error()})
+		}
+
+		if err := c.Validate(u); err != nil {
+			if validationErrors, ok := err.(validator.ValidationErrors); ok {
+				errors := make(map[string]string)
+
+				for _, fieldErr := range validationErrors {
+					errors[fieldErr.Field()] = fieldErr.Tag()
+				}
+
+				return c.JSON(http.StatusBadRequest, map[string]any{
+					"message": "Validation failed",
+					"errors":  errors,
+				})
+			}
+
+			return c.JSON(http.StatusBadRequest, map[string]any{
+				"message": err.Error(),
+			})
+		}
+
+		return c.JSON(http.StatusCreated, u)
+	})
+
 	red := color.New(color.FgRed).SprintFunc()
 
-	color.Green("🚀 Server is starting on port http://localhost:5000")
+	color.Green("🚀 Server is starting on port http://localhost:%d \n", port)
 
-	if err := e.Start(":5000"); err != nil {
+	if err := e.Start(fmt.Sprintf(":%d", port)); err != nil {
 		color.Red("❌ Failed to start server: %v", err)
 		e.Logger.Error(red(err.Error()))
 	}
